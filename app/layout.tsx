1d19c4114ffbb404246d12fb0188f50602f5e7a7
etabraham <84344154+etabraham@users.noreply.github.com>
2024-03-12 22:14:55 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 //import { Metadata } from "next";
+import Metadata from "next";
 import { Inter } from "@next/font/google";
 import "./globals.css";
 
