420347393f8f01c89145ff458c73f2a0047384da
katielaptop <katiesun138@gmail.com>
2024-03-31 11:52:00 -0700

    adding changes so now userID is unique to auth user

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+
 function Profile() {
+  const [authUser, setAuthUser] = useState<string | null>(null);
+  useEffect(()=> {
+    console.log("The authuser does NOTTTT exist")
+
+
+    if (authUser){
+      console.log("The authuser does exist")
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      var raw = JSON.stringify({"userID": authUser, "cycle": 0})
+
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw
+      }
+
+      fetch("https://pa54p2pdh7.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+      .then(response => response.text())
+      .then(result => alert(JSON.parse(result).body))
+      .catch(error => console.log("error", error))
+    }
+  }, [authUser])
+
   return (
     <Authenticator>
+      {({user }) => {
+        setAuthUser(user?.username|| null)
+        return(
+        <div>
+          <h2>HERE IS THE USERNAME AGAIN {authUser}</h2>
+        </div>
+        )
+        }}
     </Authenticator>
   );
 }
