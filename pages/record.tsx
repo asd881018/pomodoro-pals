ec4fee7aa59ad2defafd8b003a3005310b1f67ab
cta106 <cta106@sfu.ca>
2024-04-02 00:16:28 -0700

    Refactor Record component to add functionality for creating records with DB

--- pages/record.tsx
+++ pages/record.tsx
@@ -1,38 +1,84 @@
-import Layout from '../components/Layout';
-
-import React, { useState } from 'react';
-import RecordForm from '../components/RecordForm';
-import RecordList from '../components/RecordList';
-import RecordToggler from '../components/RecortToggle';
+import Layout from "../components/Layout";
+import { getCurrentUser } from "@aws-amplify/auth";
+import { CLOUDFRONT_URL } from "../utils/config";
+import React, { useState } from "react";
+import RecordForm from "../components/RecordForm";
+import RecordList from "../components/RecordList";
+import RecordToggler from "../components/RecortToggle";
 
 const Record = () => {
-	const [goals, setGoals] = useState<string[]>([]);
-	const [completedGoals, setCompletedGoals] = useState<string[]>([]);
+  const [goals, setGoals] = useState<string[]>([]);
+  const [completedGoals, setCompletedGoals] = useState<string[]>([]);
 
-	const addGoal = (text: string) => {
-		setGoals(prevGoals => [...prevGoals, text]);
-	};
+  // const addGoal = (text: string) => {
+  // 	setGoals(prevGoals => [...prevGoals, text]);
+  // };
 
-	const markAsComplete = (index: number) => {
-		const completedGoal = goals[index];
-		const updatedGoals = goals.filter((_, i) => i !== index);
-		setGoals(updatedGoals);
-		setCompletedGoals(prevCompletedGoals => [completedGoal, ...prevCompletedGoals]);
-	};
+  const addGoal = async (text: string) => {
+    // Get the current user
+    const user = await getCurrentUser();
+    const userID = user ? user.username : null;
 
-	return (
-		<Layout page={'record'}>
-			<div className='w-full h-full md:pb-14 pt-20'>
-				<div className="text-white text-xl flex items-center justify-center pb-8">Pomodoro Record</div>
+    // Data to be sent to the API
+    const postData = {
+      userID: userID,
+      completed: false,
+      goalName: text,
+    };
 
-				<div className='flex flex-col justify-between gap-2 w-11/12 m-auto text-white'>
-					<RecordToggler goals={goals} completedGoals={completedGoals} markAsComplete={markAsComplete}/>
+    // Making a POST request using fetch
+    try {
+      const response = await fetch(`${CLOUDFRONT_URL}/pomodoroRecord`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
 
-					<RecordForm onAddGoal={addGoal} />
-				</div>
-			</div>
-		</Layout>
-	);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      console.log("Success:", data);
+
+      // Update the local state only after the record has been successfully created in the backend
+      setGoals((prevGoals) => [...prevGoals, text]);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const markAsComplete = (index: number) => {
+    const completedGoal = goals[index];
+    const updatedGoals = goals.filter((_, i) => i !== index);
+    setGoals(updatedGoals);
+    setCompletedGoals((prevCompletedGoals) => [
+      completedGoal,
+      ...prevCompletedGoals,
+    ]);
+  };
+
+  return (
+    <Layout page={"record"}>
+      <div className="h-full w-full pt-20 md:pb-14">
+        <div className="flex items-center justify-center pb-8 text-xl text-white">
+          Pomodoro Record
+        </div>
+
+        <div className="m-auto flex w-11/12 flex-col justify-between gap-2 text-white">
+          <RecordToggler
+            goals={goals}
+            completedGoals={completedGoals}
+            markAsComplete={markAsComplete}
+          />
+
+          <RecordForm onAddGoal={addGoal} />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Record;
