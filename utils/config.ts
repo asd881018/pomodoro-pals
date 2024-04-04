619e9de77b223721ea458ad09466e708f320def3
Diego Buencamino <41103373+bncmn@users.noreply.github.com>
2024-04-03 23:17:16 -0700

    Update config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -1,1 +1,2 @@
-export const CLOUDFRONT_URL = 'https://d1c0fz5pado86u.cloudfront.net/dev';
+//export const CLOUDFRONT_URL = 'https://d1c0fz5pado86u.cloudfront.net/dev';
+export const CLOUDFRONT_URL = 'https://j773k0auyc.execute-api.us-west-1.amazonaws.com/dev';
